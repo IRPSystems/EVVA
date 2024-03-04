baa9b6787e896c0d94504cd6c2034bd4fbd22a9e
Smadar Tsdaka <smadar@irpsystems.com>
2024-03-04 14:16:23 +0200

    Add TestListHeightConverter

---
+++ Evva/Converter/TestListHeightConverter.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Globalization;
+using System.Windows.Data;
+using System;
+using System.Windows.Controls;
+
+namespace Evva.Converter
+{
+	public class TestListHeightConverter : IValueConverter
+	{
+
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (!(value is ListView listView))
+				return value;
+
+			double height = listView.ActualHeight - 60;
+			return height;
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return string.Empty;
+		}
+	}
+}
